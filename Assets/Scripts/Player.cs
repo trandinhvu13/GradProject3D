48853dac8e24255cff7a3ad6c8d343778d9023ac
Tran Dinh Vu <trandinhvu131299@gmail.com>
2022-05-29 22:09:22 +0700

    Done click to move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : VersionedMonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Seeker seeker;
+    [SerializeField] private AIPath aiPath;
+
+    // Input
+    private Camera cam;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+       CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, 100) && hitInfo.transform.gameObject.CompareTag("Ground"))
+            {
+                seeker.StartPath(transform.position, hitInfo.point);
+            }
+        }
     }
 }
